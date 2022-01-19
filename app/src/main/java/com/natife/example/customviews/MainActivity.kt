470cea1e3c5db5c6f41ac8e0c93a424fb65b75ca
Tornado512 <zhadkoroman@gmail.com>
2022-01-19 12:42:03 +0200

    finished custom viewGroup

--- app/src/main/java/com/natife/example/customviews/MainActivity.kt
+++ app/src/main/java/com/natife/example/customviews/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.natife.example.customviews
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val test = findViewById<CustomLinearLayout>(R.id.custom_layout)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        test.addItem("item1")
+        test.addItem("item2")
+        test.addItem("item3")
+        test.addItem("item4")
+        test.addItem("item5")
     }
+
 }
--- app/src/main/java/com/natife/example/customviews/MyViewGroup.kt
+++ app/src/main/java/com/natife/example/customviews/MyViewGroup.kt
@@ -1,13 +1,27 @@
 package com.natife.example.customviews
 
 import android.content.Context
+import android.graphics.Color
 import android.util.AttributeSet
 import android.widget.LinearLayout
+import android.widget.TextView
 
 class CustomLinearLayout @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defstyle: Int = 0
-) : LinearLayout(context, attrs, defstyle) {
+    defStyle: Int = 0
+) : LinearLayout(context, attrs, defStyle) {
+
+    private val backgroundColor = Color.parseColor(context.getString(R.string.backgrondColorCustomViewGroup))
+    private val textColor = Color.parseColor(context.getString(R.string.textColorCustomViewGroup))
+
+    fun addItem(itemName: String) {
+        val textView = TextView(context)
+        textView.text = itemName
+        textView.setBackgroundColor(backgroundColor)
+        textView.setTextColor(textColor)
+        textView.setPadding(4,4,4,4)
+        addView(textView)
+    }
 
 }
